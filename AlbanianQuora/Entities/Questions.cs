b96bb77bafdf1728925ee2cddf0a0b52f80a2848
shqipi <shqipi_r@hotmail.com>
2024-04-13 23:28:59 +0200

    Added Question entitiy and Question.cs class. ID 02

---
+++ AlbanianQuora/Entities/Questions.cs
@@ -0,0 +1,10 @@
+﻿namespace AlbanianQuora.Entities
+{
+    public class Questions
+    {
+        public int Id { get; set; }
+        public String Question { get; set; }
+        public int Like { get; set; }
+        public String AddComment { get; set; }
+    }
+}
