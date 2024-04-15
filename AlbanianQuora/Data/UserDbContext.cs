ca9eec0ebd314f3ba5dcd75c78f4f78da9d5bb3d
Arbenitamusaj <95128490+Arbenitamusaj@users.noreply.github.com>
2024-04-16 00:50:22 +0200

    Created UserDbContext class to manage user-related database operations

---
+++ AlbanianQuora/Data/UserDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AlbanianQuora.Entities;
+
+
+namespace AlbanianQuora.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        // Other user-related DbSet properties...
+    }
+}
