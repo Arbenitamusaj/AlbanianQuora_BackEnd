d42e829552e0a05c72cd3a46e701ed2611967f4d
Arbenitamusaj <95128490+Arbenitamusaj@users.noreply.github.com>
2024-04-16 00:25:31 +0200

    Implemented User Registration Model

---
+++ AlbanianQuora/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace AlbanianQuora.Entities
+
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
