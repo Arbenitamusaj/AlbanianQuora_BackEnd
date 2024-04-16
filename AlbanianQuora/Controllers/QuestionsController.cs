b0655e03f4c5d280fc30c8aff86dc8f10459c4fe
shqipi <shqipi_r@hotmail.com>
2024-04-16 14:26:18 +0200

    Added QuestionController and created 2 api HttpGet to get the qeustions and a single question -ID 04

---
+++ AlbanianQuora/Controllers/QuestionsController.cs
@@ -0,0 +1,31 @@
+﻿using AlbanianQuora.Data;
+using AlbanianQuora.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlbanianQuora.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly QuestionContext _context;
+
+        public QuestionsController(QuestionContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Question>>> GetQuestions()
+        {
+            return await _context.Questions.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Question>> GetQuestion(int id)
+        {
+            return await _context.Questions.FindAsync(id);
+        }
+    }
+}
