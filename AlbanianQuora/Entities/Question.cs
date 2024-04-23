a89262274432f6fc92e4908172c6fbb14fe667c7
Arbenitamusaj <95128490+Arbenitamusaj@users.noreply.github.com>
2024-04-23 08:18:19 +0200

    Retry merge

--- AlbanianQuora/Entities/Question.cs
+++ AlbanianQuora/Entities/Question.cs
@@ -6,11 +6,12 @@ namespace AlbanianQuora.Entities
         public Guid Id { get; set; } = Guid.NewGuid();
         public string QuestionTitle { get; set; }
         public string QuestionDescription { get; set; }
-        public Guid QuestionCategoryId { get; set; }
-        public QuestionCategory QuestionCategory { get; set; }
         public Guid UserId { get; set; }
         public User User { get; set; }
+        public Guid QuestionCategoryId { get; set; }
+        public QuestionCategory QuestionCategory { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
     }
 }
 
