a59a385a2b5ff9f5076edd1a958ea8aa78c4ba16
Arbenitamusaj <95128490+Arbenitamusaj@users.noreply.github.com>
2024-04-25 02:53:27 +0200

    Comment get DTO

---
+++ AlbanianQuora/DTO/CommentGetDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace AlbanianQuora.DTO
+{
+    public class CommentGetDTO
+    {
+        public string Content { get; set; }
+        public string UserName { get; set; }
+        public string TimeAgo { get; set; }
+    }
+}
